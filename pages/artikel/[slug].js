df415e88709e38ee0a6e95c8c22c3d0154114a4f
nayakayp <nayakayp@gmail.com>
2022-06-28 20:56:06 +0700

    blog content styling fixed

--- pages/artikel/[slug].js
+++ pages/artikel/[slug].js
@@ -10,92 +10,76 @@ const prism = require("prismjs");
 require("prismjs/components/prism-python");
 
 export async function getStaticProps({ params: { slug } }) {
-	const postDetailQuery = `*[_type == "post" && slug.current == "${slug}"][0]`;
-	const postDetail = await client.fetch(postDetailQuery);
+    const postDetailQuery = `*[_type == "post" && slug.current == "${slug}"][0]`;
+    const postDetail = await client.fetch(postDetailQuery);
 
-	return {
-		props: {
-			postDetail,
-		},
-	};
+    return {
+        props: {
+            postDetail,
+        },
+    };
 }
 
 export async function getStaticPaths() {
-	const query = `*[_type == "post"]{
+    const query = `*[_type == "post"]{
 		slug{
 			current
 		}
 	}`;
-	const posts = await client.fetch(query);
-	const paths = posts.map((post) => ({
-		params: {
-			slug: post.slug.current,
-		},
-	}));
+    const posts = await client.fetch(query);
+    const paths = posts.map((post) => ({
+        params: {
+            slug: post.slug.current,
+        },
+    }));
 
-	return {
-		paths,
-		fallback: "blocking",
-	};
+    return {
+        paths,
+        fallback: "blocking",
+    };
 }
 
 const components = {
-	types: {
-		code: (props) => (
-			<pre data-language={props.node.language}>
-				<code>{props.node.code}</code>
-			</pre>
-		),
-	},
+    types: {
+        code: (props) => {
+            console.log(props);
+            return (
+                <pre className="relative rounded-lg">
+                    <code className="language-javascript">
+                        {props.value.code}
+                    </code>
+                </pre>
+            );
+        },
+    },
 };
 
 export default function Artikel({ postDetail }) {
-	const { title, date, content } = postDetail;
-	useEffect(() => {
-		prism.highlightAll();
-	}, []);
-	return (
-		<div className={styles.root}>
-			<Head>
-				<title>{title}</title>
-			</Head>
-			<article className="">
-				<h1 className="mb-2 text-3xl font-bold text-dark-400 md:mb-4 md:text-4xl">
-					{title}
-				</h1>
-				<p className="mb-4 text-base text-dark-100 md:mb-9">
-					<Date dateString={date} />
-				</p>
-				<pre>
-					<code className="language-javascript">
-						{`import "prismjs/themes/prism-tomorrow.css";
+    const { title, date, content } = postDetail;
+    console.log(content);
+    useEffect(() => {
+        prism.highlightAll();
+    }, []);
+    return (
+        <div className={styles.root}>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <article className="">
+                <h1 className="mb-2 text-3xl font-bold text-dark-400 md:mb-4 md:text-4xl">
+                    {title}
+                </h1>
+                <p className="mb-4 text-base text-dark-100 md:mb-9">
+                    <Date dateString={date} />
+                </p>
 
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
-
-	const nayaka= 23;
-	const nayaka= '23'
-	const nayaka= {
-		name:nayaka,
-		age:23
-	}
-}`}
-					</code>
-				</pre>
-				<div className="articleBody text-lg">
-					<PortableText value={content} components={components} />
-				</div>
-				{/* <div
-					className="articleBody text-lg"
-					dangerouslySetInnerHTML={{ __html: content }}
-				/> */}
-			</article>
-		</div>
-	);
-}
-
-function blocksToText(blocks) {
-	return blocks.map((block) =>
-		block.children.map((child) => child.text).join("")
-	);
+                {/*<pre className="relative">
+                    <code className="language-javascript">{``}</code>
+                </pre>*/}
+                <div className="articleBody text-lg">
+                    <PortableText value={content} components={components} />
+                </div>
+            </article>
+        </div>
+    );
 }
